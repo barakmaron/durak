3bb4326d7edd25502c68fd8a82a9f8808dac5296
BarakMaron <barak062@gmail.com>
2023-02-02 16:05:15 +0200

    feat: divider genric component

---
+++ client/src/components/Divider/Divider.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Divider = ({
+    children
+}) => {
+  return (<div className="relative flex py-5 items-center w-full px-2">
+    <div className="flex-grow border-t border-gray-400"></div>
+    <span className="flex-shrink mx-4 text-gray-400">{children}</span>
+    <div className="flex-grow border-t border-gray-400"></div>
+    </div>);
+};
+
+export default Divider;
