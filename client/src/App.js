f40624b2bc9c389c6dd0aacf67e3aa4ae5daf31b
BarakMaron <barak062@gmail.com>
2023-02-02 16:03:50 +0200

    feat: adds store and routes render

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
+import React, { Suspense } from 'react';
+import  { Routes, Route } from 'react-router-dom';
+import Helmet from 'react-helmet';
+import routes from './routes';
+import Loader from './components/Loader/Loader';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+  return (<>
+    <Routes>
+      {routes.map((route) => {
+        return <React.Fragment key={route.location}>
+            <Route
+            path={route.location}
+            element={<>
+              <Helmet>
+                <title>{route.title}</title>
+              </Helmet>
+              <Suspense fallback={<Loader/>}>
+                <route.element { ...(route.props || [])}></route.element>
+              </Suspense>
+            </>}/>
+        </React.Fragment>;
+      })}
+    </Routes>
+  </>);
 }
 
 export default App;
