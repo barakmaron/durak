11fe15a861ae9f376d31ecaec1191fcf91e2fe4c
BarakMaron <barak062@gmail.com>
2023-02-02 18:51:41 +0200

    feat: basic express server

---
+++ server/app.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import routes from './routes/index.js';
+import ErrorHandler from './middleware/ErrorHandler.js';
+import ValidationErrorMiddleware from './middleware/ValidationErrorMiddleware.js';
+import { sequelize } from './db/models/index.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename); 
+
+Promise.resolve(sequelize.sync({  })).then(() => {
+});
+
+const app = express();
+app.use(cookieParser());
+app.use([morgan("common"), cors({ origin:true, credentials: true }), express.json(), express.urlencoded()]);
+
+app.use('/', routes);
+app.use(ValidationErrorMiddleware);
+
+// app.use(express.static(path.join(__dirname, '../client/build')));
+// app.get('(/*)?', function(req, res) {
+//   res.sendFile('index.html', {root: path.join(__dirname, '../client/build/')});
+// });
+
+// app.use(ErrorHandler);
+// process.on('unhandledRejection', (reason, promise) => {
+//   console.log('Uncaught Rejection', reason.message);
+//   throw reason;
+// });
+
+// process.on('uncaughtException', (error) => {
+//   console.log("Uncaught Exception", error.message);
+//   process.exit(1);
+// });
+
+export default app;
